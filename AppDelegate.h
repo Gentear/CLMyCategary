f031d98252b45671ed97400759288bbd90a06a9a
zyyt <zyyt@MacBook-Pro-2.local>
2016-09-22 16:18:42 +0800

    强大的分类
    一个强大的category，将会不断更新

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CLMyCategary
+//
+//  Created by zyyt on 16/9/22.
+//  Copyright © 2016年 conglei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CLMyCategary
+//
+//  Created by zyyt on 16/9/22.
+//  Copyright © 2016年 conglei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
