3dde8ae4ed576bb941d46cbf5b56c9c5e23878e6
zyyt <zyyt@MacBook-Pro-2.local>
2016-10-10 14:16:56 +0800

    增加textView的分类
    placeholder

---
+++ CLMyCategary/UIKit/UITextView/UITextView+CLTextView.h
@@ -0,0 +1,29 @@
+//
+//  UITextView+CLTextView.h
+//  CLMyCategary
+//
+//  Created by zyyt on 16/10/10.
+//  Copyright © 2016年 conglei. All rights reserved.
+//
+
+//#import <UIKit/UIKit.h>
+//
+//@interface UITextView (CLTextView)
+//
+//@end
+@import UIKit;
+
+FOUNDATION_EXPORT double UITextView_PlaceholderVersionNumber;
+FOUNDATION_EXPORT const unsigned char UITextView_PlaceholderVersionString[];
+
+@interface UITextView (CLTextView)
+
+@property (nonatomic, readonly) UILabel *placeholderLabel;
+
+@property (nonatomic, strong) IBInspectable NSString *placeholder;
+@property (nonatomic, strong) NSAttributedString *attributedPlaceholder;
+@property (nonatomic, strong) IBInspectable UIColor *placeholderColor;
+
++ (UIColor *)defaultPlaceholderColor;
+
+@end
